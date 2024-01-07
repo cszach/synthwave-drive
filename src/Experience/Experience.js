9e9e39bbe096383d4fc048c8b407d756de23ba1b
Zach <duong.nguyen@rochester.edu>
2024-01-07 14:43:12 -0500

    ｆｏｌｄｅｒｓ　ａｒｅ　ｌｏｗｅｒｃａｓｅ　せ果イ

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,11 +1,11 @@
 import * as THREE from "three";
-import Sizes from "./Utils/Sizes";
-import Time from "./Utils/Time";
+import Sizes from "./utils/Sizes";
+import Time from "./utils/Time";
 import Camera from "./Camera";
 import Renderer from "./Renderer";
-import World from "./World/World";
-import Resources from "./Utils/Resources";
-import Debug from "./Utils/Debug";
+import World from "./world/World";
+import Resources from "./utils/Resources";
+import Debug from "./utils/Debug";
 import sources from "./sources";
 
 let instance = null;
