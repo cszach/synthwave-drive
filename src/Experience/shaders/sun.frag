75a651a60796df0821efd3453e99acdf88e99615
Zach <duong.nguyen@rochester.edu>
2024-01-11 18:32:02 -0500

    [ｓｕｎ]　ｏｎｅ　ｇａｐ　＋　ｄｅｂｕｇ　ｃｏｎｔｒｏｌｓ　為ギ゙へ旺ぶ

--- src/Experience/shaders/sun.frag
+++ src/Experience/shaders/sun.frag
@@ -1,8 +1,15 @@
+uniform float gapStart;
+uniform float initialGapSize;
+
 varying vec2 vUv;
 
 void main() {
 	float strength = step(distance(vUv, vec2(0.5)), 0.2);
 	float alpha = strength == 0.0 ? 0.0 : 1.0;
+
+	if (vUv.y < gapStart && vUv.y > gapStart - initialGapSize) {
+		alpha = 0.0;
+	}
 
 	gl_FragColor = vec4(vec3(strength), alpha);
 }
--- src/Experience/world/Sun.js
+++ src/Experience/world/Sun.js
@@ -10,9 +10,16 @@ export default class Sun {
     this.time = this.experience.time;
     this.debug = this.experience.debug;
 
+    // TODO: Move config to parent
+    this.config = {
+      gapStart: 0.6,
+      initialGapSize: 0.05,
+    };
+
     this.setGeometry();
     this.setMaterial();
     this.setMesh();
+    if (this.debug.active) this.setDebug();
   }
 
   setGeometry() {
@@ -24,6 +31,10 @@ export default class Sun {
       vertexShader: sunVertexShader,
       fragmentShader: sunFragmentShader,
       transparent: true,
+      uniforms: {
+        gapStart: { value: this.config.gapStart },
+        initialGapSize: { value: this.config.initialGapSize },
+      },
     });
   }
 
@@ -33,6 +44,17 @@ export default class Sun {
     this.mesh.rotation.x = Math.PI;
 
     this.scene.add(this.mesh);
+  }
+
+  setDebug() {
+    this.debugFolder = this.debug.ui.addFolder("Sun");
+
+    this.debugFolder
+      .add(this.material.uniforms.gapStart, "value", 0.0, 1.0, 0.01)
+      .name("gapStart");
+    this.debugFolder
+      .add(this.material.uniforms.initialGapSize, "value", 0.0, 1.0, 0.01)
+      .name("initialGapSize");
   }
 
   update() {}
