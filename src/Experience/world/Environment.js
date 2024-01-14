33a024b05a14e976acb0fc82f8f463ad15e1659d
Zach <duong.nguyen@rochester.edu>
2024-01-13 19:42:51 -0500

    [ｅｎｖｉｒｏｎｍｅｎｔ]：　ａｄｄ　ｓｋｙ　応淫疫

--- src/Experience/world/Environment.js
+++ src/Experience/world/Environment.js
@@ -1,14 +1,17 @@
 import * as THREE from "three";
 import Experience from "../Experience";
+import colorPalette from "./colorPalette";
 
 export default class Environment {
   constructor() {
     this.experience = new Experience();
     this.scene = this.experience.scene;
     this.resources = this.experience.resources;
+    this.colorPalette = colorPalette;
     this.debug = this.experience.debug;
 
     this.setSunLight();
+    this.setSky();
     if (this.debug.active) this.setDebug();
   }
 
@@ -22,6 +25,48 @@ export default class Environment {
     this.sunLight.position.set(3.5, 2, -1.25);
 
     this.scene.add(this.sunLight);
+  }
+
+  setSky() {
+    this.skyGeometry = new THREE.SphereGeometry(10000, 25, 25);
+    this.skyMaterial = new THREE.ShaderMaterial({
+      uniforms: {
+        skyStart: { value: 0.8 },
+        skyEnd: { value: 0.2 },
+        topColor: { value: new THREE.Color(this.colorPalette.night) },
+        bottomColor: { value: new THREE.Color(this.colorPalette.fuchsia) },
+      },
+      vertexShader: `
+        varying vec2 vUv;
+
+        void main() {
+          gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+
+          vUv = uv;
+        }
+      `,
+      fragmentShader: `
+        uniform vec3 topColor;
+        uniform vec3 bottomColor;
+        uniform float skyStart;
+        uniform float skyEnd;
+        varying vec2 vUv;
+
+        void main() {
+          vec3 color = (vUv.y > skyStart)
+            ? topColor
+            : (vUv.y < skyEnd
+              ? bottomColor
+              : mix(bottomColor, topColor, (vUv.y - skyEnd) / (skyStart - skyEnd)));
+
+          gl_FragColor = vec4(color, 1.0);
+        }
+      `,
+      side: THREE.BackSide,
+    });
+
+    this.sky = new THREE.Mesh(this.skyGeometry, this.skyMaterial);
+    this.scene.add(this.sky);
   }
 
   setDebug() {
@@ -42,8 +87,16 @@ export default class Environment {
       .name("sunLightZ");
 
     this.debugFolder
-      .add(this.environmentMap, "intensity", 0, 4, 0.001)
-      .name("envMapIntensity")
-      .onChange(this.environmentMap.updateMaterials);
+      .add(this.skyMaterial.uniforms.skyStart, "value", 0.0, 1.0, 0.01)
+      .name("skyStart");
+    this.debugFolder
+      .add(this.skyMaterial.uniforms.skyEnd, "value", 0.0, 1.0, 0.01)
+      .name("skyEnd");
+    this.debugFolder
+      .addColor(this.skyMaterial.uniforms.topColor, "value")
+      .name("topColor");
+    this.debugFolder
+      .addColor(this.skyMaterial.uniforms.bottomColor, "value")
+      .name("bottomColor");
   }
 }
