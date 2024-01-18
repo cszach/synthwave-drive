ca676197d790d3eafc67eca8291d145426e81dfd
Zach <duong.nguyen@rochester.edu>
2024-01-18 13:03:57 -0500

    [ｅｎｖｉｒｏｎｍｅｎｔ]　ｇｅｔ　ｒｉｄ　ｏｆ　ｔｈｅ　ｏｌｄ　ｌｉｇｈｔ　ゅえニラ可仮ュ

--- src/Experience/world/Environment.js
+++ src/Experience/world/Environment.js
@@ -12,21 +12,8 @@ export default class Environment {
     this.colorPalette = this.experience.colorPalette;
     this.debug = this.experience.debug;
 
-    this.setSunLight();
     this.setSky();
     if (this.debug.active) this.setDebug();
-  }
-
-  setSunLight() {
-    this.sunLight = new THREE.DirectionalLight("#ffffff", 4);
-
-    this.sunLight.castShadow = true;
-    this.sunLight.shadow.camera.far = 15;
-    this.sunLight.shadow.mapSize.set(1024, 1024);
-    this.sunLight.shadow.normalBias = 0.05;
-    this.sunLight.position.set(3.5, 2, -1.25);
-
-    this.scene.add(this.sunLight);
   }
 
   setSky() {
@@ -54,19 +41,6 @@ export default class Environment {
 
   setDebug() {
     this.debugFolder = this.debug.ui.addFolder("Environment");
-
-    this.debugFolder
-      .add(this.sunLight, "intensity", 0, 10, 0.001)
-      .name("sunLightIntensity");
-    this.debugFolder
-      .add(this.sunLight.position, "x", -5, 5, 0.001)
-      .name("sunLightX");
-    this.debugFolder
-      .add(this.sunLight.position, "y", -5, 5, 0.001)
-      .name("sunLightY");
-    this.debugFolder
-      .add(this.sunLight.position, "z", -5, 5, 0.001)
-      .name("sunLightZ");
 
     this.debugFolder
       .add(this.skyMaterial.uniforms.skyStart, "value", 0.0, 1.0, 0.01)
