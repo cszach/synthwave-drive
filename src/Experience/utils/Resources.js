5c1063f51296adf4d986afeee424a3a62530aba3
Zach <duong.nguyen@rochester.edu>
2024-01-21 11:31:30 -0500

    [ｒｅｓｏｕｒｃｅｓ]：　ｓｕｐｐｏｒｔ　ｌｏａｄｉｎｇ　ｏｆ　ｏｂｊ　ｍｏｄｅｌｓ　よ汚ヲ位ぇ員

--- src/Experience/utils/Resources.js
+++ src/Experience/utils/Resources.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import Experience from "../Experience";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import EventEmitter from "./EventEmitter";
 
@@ -34,11 +35,16 @@ export default class Resources extends EventEmitter {
       button.classList.add("activated");
       this.trigger("ready");
     };
+
+    THREE.DefaultLoadingManager.onError = (itemUrl) => {
+      console.error(`Error loading resource at ${itemUrl}.`);
+    };
   }
 
   setLoaders() {
     this.loaders = {
       gltfLoader: new GLTFLoader(),
+      objLoader: new OBJLoader(),
       textureLoader: new THREE.TextureLoader(),
       cubeTextureLoader: new THREE.CubeTextureLoader(),
     };
@@ -51,18 +57,30 @@ export default class Resources extends EventEmitter {
 
   startLoading() {
     this.sources.forEach((source) => {
-      if (source.type === "gltfModel") {
-        this.loaders.gltfLoader.load(source.path, (file) => {
-          this.items[source.name] = file;
-        });
-      } else if (source.type === "texture") {
-        this.loaders.textureLoader.load(source.path, (file) => {
-          this.items[source.name] = file;
-        });
-      } else if (source.type === "cubeTexture") {
-        this.loaders.cubeTextureLoader.load(source.path, (file) => {
-          this.items[source.name] = file;
-        });
+      switch (source.type) {
+        case "gltfModel":
+          this.loaders.gltfLoader.load(source.path, (file) => {
+            this.items[source.name] = file;
+          });
+          break;
+
+        case "objModel":
+          this.loaders.objLoader.load(source.path, (file) => {
+            this.items[source.name] = file;
+          });
+          break;
+
+        case "texture":
+          this.loaders.textureLoader.load(source.path, (file) => {
+            this.items[source.name] = file;
+          });
+          break;
+
+        case "cubeTexture":
+          this.loaders.cubeTextureLoader.load(source.path, (file) => {
+            this.items[source.name] = file;
+          });
+          break;
       }
     });
   }
