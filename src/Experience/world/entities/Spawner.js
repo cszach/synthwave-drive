99dd25e60b54377c85b66b27f631058e1f7a09a9
Zach <duong.nguyen@rochester.edu>
2024-01-23 07:29:39 -0500

    [ｗｏｒｌｄ]　ｍｏｖｅ　ｔｒｅｅ　ｓｐａｗｎｅｒ　ｔｏ　ｉｔｓ　ｏｗｎ　ｆｉｌｅ　ま尉るの遠益唄ッ　

--- src/Experience/world/entities/Spawner.js
+++ src/Experience/world/entities/Spawner.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import Experience from "./Experience";
+import Experience from "../../Experience";
 import { gsap } from "gsap/gsap-core";
 
 /**
---
+++ src/Experience/world/entities/TreeSpawner.js
@@ -0,0 +1,26 @@
+import Experience from "../../Experience";
+import Spawner from "./Spawner";
+import Tree from "../Tree";
+
+export default class TreeSpawner {
+  static new() {
+    const experience = new Experience();
+    const cubeCamera = experience.cubeCamera;
+    const car = experience.world.car;
+    const tree = new Tree();
+
+    return new Spawner(
+      [tree.geometry],
+      [tree.material, tree.wireframeMaterial],
+      car.position,
+      {
+        count: 42,
+        triggerRadius: 150,
+        generationRadius: 300,
+        yStart: -20,
+        yEnd: 0,
+      },
+      [cubeCamera.layerNumber]
+    );
+  }
+}
