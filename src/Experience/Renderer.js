f8ddd4f3a5acd0fc9ae7249a1457b98c42a87bb6
Zach <duong.nguyen@rochester.edu>
2024-01-24 12:28:43 -0500

    [ｐｏｓｔｐｒｏｃｅｓｓｉｎｇ]：　ａｎｔｉａｌｉａｓｉｎｇ　亜閲内

--- src/Experience/Renderer.js
+++ src/Experience/Renderer.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import Experience from "./Experience";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
+import { SMAAPass } from "three/examples/jsm/postprocessing/SMAAPass";
 
 export default class Renderer {
   constructor() {
@@ -18,7 +19,7 @@ export default class Renderer {
   setInstance() {
     this.instance = new THREE.WebGLRenderer({
       canvas: this.canvas,
-      // antialias: true,
+      antialias: true,
     });
     this.instance.toneMapping = THREE.CineonToneMapping;
     this.instance.toneMappingExposure = 1.75;
@@ -29,15 +30,38 @@ export default class Renderer {
   }
 
   setPostProcessing() {
-    this.effectComposer = new EffectComposer(this.instance);
+    this.renderTarget = new THREE.WebGLRenderTarget(800, 600, {
+      samples: this.instance.getPixelRatio() === 1 ? 3 : 0,
+    });
+
+    console.log(this.renderTarget);
+
+    this.effectComposer = new EffectComposer(this.instance, this.renderTarget);
+    this.effectComposer.setSize(this.sizes.width, this.sizes.height);
+    this.effectComposer.setPixelRatio(this.sizes.pixelRatio);
+
+    // Render pass
 
     this.renderPass = new RenderPass(this.scene, this.camera.instance);
     this.effectComposer.addPass(this.renderPass);
+
+    // Anti-aliasing pass, only if the browser does not support WebGL 2.
+
+    if (
+      this.instance.getPixelRatio() === 1 &&
+      !this.instance.capabilities.isWebGL2
+    ) {
+      this.smaaPass = new SMAAPass();
+      this.effectComposer.addPass(this.smaaPass);
+    }
   }
 
   resize() {
     this.instance.setSize(this.sizes.width, this.sizes.height);
     this.instance.setPixelRatio(this.sizes.pixelRatio);
+
+    this.effectComposer.setSize(this.sizes.width, this.sizes.height);
+    this.effectComposer.setPixelRatio(this.sizes.pixelRatio);
   }
 
   update() {
--- src/Experience/utils/Sizes.js
+++ src/Experience/utils/Sizes.js
@@ -12,7 +12,7 @@ export default class Sizes extends EventEmitter {
     window.addEventListener("resize", () => {
       this.width = window.innerWidth;
       this.height = window.innerHeight;
-      this.pixelRatio = Math.min(window.devicePixelRatio);
+      this.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
       this.trigger("resize");
     });
