547c6a92822380d36b70d7f1a96f8276555a5930
Zach <duong.nguyen@rochester.edu>
2024-01-25 13:19:57 -0500

    [ａｕｄｉｏ]：　ａｄｄ　ｓｏｍｅ　ａｕｄｉｏ　ｆｉｌｅｓ　ぼ易隠ク゛

--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -9,4 +9,29 @@ export default [
     type: "objModel",
     path: "models/Tree/palm_tree_tall.obj",
   },
+  {
+    name: "bedtime-stories.mp3",
+    type: "audio",
+    path: "audio/music/mezhdunami-bedtime-stories-1200.mp3",
+  },
+  {
+    name: "drive.mp3",
+    type: "audio",
+    path: "audio/music/mezhdunami-drive-120288.mp3",
+  },
+  {
+    name: "fading-echoes.mp3",
+    type: "audio",
+    path: "audio/music/mezhdunami-fading-echoes-129291.mp3",
+  },
+  {
+    name: "the-shining.mp3",
+    type: "audio",
+    path: "audio/music/mezhdunami-the-shining-1199.mp3",
+  },
+  {
+    name: "voyager.mp3",
+    type: "audio",
+    path: "audio/music/mezhdunami-voyager-141276.mp3",
+  },
 ];
