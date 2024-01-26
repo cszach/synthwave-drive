ecc3033e68ede7adfb360845fb9da84e1c2ea629
Zach <duong.nguyen@rochester.edu>
2024-01-25 23:33:28 -0500

    [ｒｅｎｄｅｒｅｒ]：　ｄｉｓａｂｌｅ　ｓｈａｄｏｗｓ　押円鋭

--- src/Experience/Renderer.js
+++ src/Experience/Renderer.js
@@ -27,8 +27,6 @@ export default class Renderer {
     });
     this.instance.toneMapping = THREE.CineonToneMapping;
     this.instance.toneMappingExposure = 1.75;
-    this.instance.shadowMap.enabled = true;
-    this.instance.shadowMap.type = THREE.PCFSoftShadowMap;
     this.instance.setSize(this.sizes.width, this.sizes.height);
     this.instance.setPixelRatio(this.sizes.pixelRatio);
   }
